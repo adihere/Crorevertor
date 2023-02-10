e7f5187fa87e43d85a3f3de4505c4860ba4fa736
smartwayswork <smartwayswork@gmail.com>
2023-02-10 16:16:40 +0000

    simple - works with fixer API

--- convertor.js
+++ convertor.js
@@ -2,8 +2,27 @@
 
 var trial1 = config.TRIAL_TOKEN1;
 var trial2 = config.TRIAL_SECRET_KEY1;
+var fixerAPI = config.FIXER_API_KEY;
 
 function clicked(){
-    alert('TRIAL_TOKEN1 is ' + trial1);
+
+    alert('fixer_API_TOKEN1 is ' + fixerAPI);
+
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", fixerAPI);
+
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+    };
+
+    
+fetch('https://api.apilayer.com/fixer/latest?base=USD&symbols=EUR,GBP', requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
 }
+
+
 
